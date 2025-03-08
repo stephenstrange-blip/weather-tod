c1421e627d96eb7e88c911f852d118e8d1808292
jade <121040221+stephenstrange-blip@users.noreply.github.com>
2025-03-09 06:54:35 +0800

    Build: create api for retrieving data from external API

---
+++ src/components/api.js
@@ -0,0 +1,52 @@
+const completeFormat =
+  "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/[location]/[date1]/[date2]?key=YOUR_API_KEY";
+
+export class Weather {
+  #apiKeyVisualCrossing = "JN9KW5H3RUM2E9KWBBJ4CHCQM";
+  #baseTimelineUrl =
+    "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline";
+
+
+  async fetchData(query) {
+    try {
+      const response = await fetch(query, {
+        mode: "cors",
+        method: "GET",
+        "headers" : {}
+      });
+
+      const obj = await response.json();
+			return obj;
+
+    } catch (err) {
+      throw Error(err);
+    }
+  }
+
+  setQuery(location, date1, date2) {
+    let query;
+		let base = this.#baseTimelineUrl;
+
+    if (!date1) query = base + `/${location}`;
+    if (!!date1 && !date2) query = base + `/${location}/${date1}`;
+    if (!!date1 && !!date2) query = base + `/${location}/${date1}/${date2}`;
+
+    query = query + `?key=${this.#apiKeyVisualCrossing}&contentType=json`;
+    return query;
+  }
+
+  setDate(year, month, day) {
+    return `${year}-${month}-${day}`;
+  }
+};
+
+export class Response {
+
+	static getHumidity(day){
+			return day.humidity
+	}
+
+	static getCondition(day){
+			return day.conditions
+	}
+}
