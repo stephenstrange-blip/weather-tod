426171120fa2b4aa0dee6e1db7fd4722cf014704
jade <121040221+stephenstrange-blip@users.noreply.github.com>
2025-03-10 19:27:39 +0800

    Chore: create session calls to main script

--- src/index.js
+++ src/index.js
@@ -1,26 +1,8 @@
-import { Weather } from "./components/api";
-import { Response } from "./components/api";
-import { Form, DataContainer } from "./components/ui";
 import './css_reset.css'
 import './styles.css'
+import { Session } from './components/session'
 
-// const weather = new Weather();
-// const date1 = weather.setDate("2025","08","19");
-// const query = weather.setQuery("budapest", date1);
-// const obj = await weather.fetchData(query);
+const session = new Session();
+// session.handleSubmit();
 
-// console.log(Response.getHumidity(obj.days[0]))
-// console.log(Response.getCondition(obj.days[0]))
-const width = window.innerWidth;
-const height = window.innerHeight;
-console.log(width, height);
 
-const bodyContainer = document.createElement("div");
-bodyContainer.classList.add("body-container");
-bodyContainer.style.width = `${width}px`;
-bodyContainer.style.height = `${height}px`;
-
-Form.setup(bodyContainer);
-DataContainer.setup(bodyContainer);
-document.body.append(bodyContainer);
-Form.handleSubmit();
